6ca291208ae4559c2bda1b3359a146926105a870
RainBWP <114127654+RainBWP@users.noreply.github.com>
2024-11-14 01:34:06 -0600

    Se ajusta a nuevas necesidades del programa para parcial 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
-// App.tsx
-import './MemoryManager.tsx';
 import './App.css';
 import './interfaces.tsx';
 import ImportFile from './ImportFile.tsx';
 import { useState } from 'react';
 import { EstructuraDeMemoria, MemoriaFisica } from './interfaces.tsx';
 import MemoryForm from './MemoryForm.tsx';
-import { TraductorDVaDF } from './Traductor.tsx';
+
+const convertirAPequenos = (valor: number) => {
+  return valor * 1024;
+  
+};
 
 const defaultMemory: EstructuraDeMemoria = {
   tamano_de_pagina: 0,
-  cantidad_de_marcos: 0,
-  cantidad_de_paginas: 0,
+  tamano_de_virtual: 0,
+  tamano_de_fisica: 0,
+  numero_de_paginas: 0,
 };
 
 const useStorage = () => {
@@ -42,36 +45,33 @@ function App() {
 
       <ImportFile onImport={set} />
 
-      <TraductorDVaDF 
-      MemoriaVirtual={MemoriaFisicaLocal.memoryValues} 
-      EstructuraDeMemoria={EstructuraDeMemoriaLocal} />
+      
 
       <div>
         <h2>Memoria Estructura</h2>
-        <p><b>Tamano De Pagina: </b>{EstructuraDeMemoriaLocal.tamano_de_pagina}</p>
-        <p><b>Cantidad De Marcos: </b>{EstructuraDeMemoriaLocal.cantidad_de_marcos}</p>
-        <p><b>Cantidad De Paginas: </b>{EstructuraDeMemoriaLocal.cantidad_de_paginas}</p>
+        <p><b>Tamano De Pagina: </b>{EstructuraDeMemoriaLocal.tamano_de_pagina} bytes</p>
+        <p><b>Tamano de Memoria Virtual: </b>{EstructuraDeMemoriaLocal.tamano_de_virtual} kb = {convertirAPequenos(EstructuraDeMemoriaLocal.tamano_de_virtual)} bytes = {Math.log2(convertirAPequenos(EstructuraDeMemoriaLocal.tamano_de_virtual))} bits</p>
+        <p><b>Tamano de Memoria Fisica: </b>{EstructuraDeMemoriaLocal.tamano_de_fisica} kb = {convertirAPequenos(EstructuraDeMemoriaLocal.tamano_de_fisica)} bytes = {Math.log2(convertirAPequenos(EstructuraDeMemoriaLocal.tamano_de_fisica))} bits</p>
+        <p><b>Numero de Paginas: </b>{EstructuraDeMemoriaLocal.numero_de_paginas}</p>
         <h2>Memoria Fisica</h2>
         <table>
-          <tr>
-                  <th>Num</th>
-                  <th>A</th>
-                  <th>R</th>
-                  <th>M</th>
-                  <th>P</th>
-                  <th>C</th>
-                  <th>Frame</th>
-                  <th>F Decimal</th>
-                  <th>Memoria</th>
-          </tr>
-            {MemoriaFisicaLocal.memoryValues.slice(0, -1).map((value, index) => (
-            <tr key={index}>
-              <th>{index}</th>
-              <MemoryForm memoryValue={value} memoryEstructure={EstructuraDeMemoriaLocal} />
+          <thead>
+            <tr>
+              <th>No</th>
+              <th>Binario</th>
+              <th>Decimal</th>
+              <th>Hexadecimal</th>
             </tr>
+          </thead>
+          <tbody>
+            {MemoriaFisicaLocal.memoryValues.map((value, index) => (
+              <tr key={index}>
+                <td><b>{index}</b></td>
+                <MemoryForm memoryValue={value} memoryEstructure={EstructuraDeMemoriaLocal} />
+              </tr>
             ))}
+          </tbody>
         </table>
-        
       </div>
     </>
   );
