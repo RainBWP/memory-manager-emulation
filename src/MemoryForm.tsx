03bdffc9273fa126da7ff222d94ab8ae61a5b9bc
RainBWP <114127654+RainBWP@users.noreply.github.com>
2024-11-14 01:34:57 -0600

    Nueva forma de manejo de lectura de bytes

--- src/MemoryForm.tsx
+++ src/MemoryForm.tsx
@@ -2,39 +2,33 @@ import React from 'react';
 import { MemoryFormProps } from './interfaces';
 
 const MemoryForm: React.FC<MemoryFormProps> = ({ memoryValue, memoryEstructure }) => {
-  // Transformation logic
-  const bitsToShift = Math.ceil(Math.log2(memoryEstructure.cantidad_de_marcos));
+
+  // Número de marcos en la memoria física
+  const numeroDeMarcos =  Math.log2(memoryEstructure.tamano_de_virtual*1024) - Math.log2(memoryEstructure.tamano_de_fisica*1024);
+
+  // Bits necesarios para representar los marcos de página
+  const bitsToShift = numeroDeMarcos;
+
   console.log("Bits que mueve", bitsToShift);
   console.log("Valor de Memoria 2: ", memoryValue.toString(2));
 
-  const MemoryReader = {
-    bit_permiso: (memoryValue >> (bitsToShift + 5)) & 1,
-    bit_referencia: (memoryValue >> (bitsToShift + 4)) & 1,
-    bit_modificado: (memoryValue >> (bitsToShift + 3)) & 1,
-    bit_presente_ausente: (memoryValue >> (bitsToShift + 2)) & 1,
-    bit_cache: (memoryValue >> (bitsToShift + 1)) & 1,
-    numero_de_frame: memoryValue & ((1 << bitsToShift) - 1),
-  };
-
-  const stringOfBits = () => {
+  const stringOfBits = (number:number) => {
     let frameString = "";
-    const numero_de_frame = memoryValue & ((1 << bitsToShift) - 1);
-    for (let i = bitsToShift - 1; i >= 0; i--) {
-      frameString += (numero_de_frame >> i) & 1 ? "1" : "0";
+    for (let i = Math.log2(memoryEstructure.tamano_de_pagina) - 1; i >= 0; i--) {
+      frameString += (number >> i) & 1 ? "1" : "0";
     }
     return frameString;
   };
 
+  const stringOfHexa = (number:number) => {
+    return number.toString(16);
+  }
+
   return (
     <>
-      <td>{MemoryReader.bit_permiso}</td>
-      <td>{MemoryReader.bit_referencia}</td>
-      <td>{MemoryReader.bit_modificado}</td>
-      <td>{MemoryReader.bit_presente_ausente}</td>
-      <td>{MemoryReader.bit_cache}</td>
-      <td>{stringOfBits()}</td>
-      <td>{MemoryReader.numero_de_frame}</td>
+      <td>{stringOfBits(memoryValue)}</td>
       <td>{memoryValue}</td>
+      <td>{stringOfHexa(memoryValue)}</td>
     </>
   );
 };
