c5d436086a378c214d2dc187f28f0a8fc57ce236
RainBWP <114127654+RainBWP@users.noreply.github.com>
2024-11-14 01:57:34 -0600

    Se quita la dependencia

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import './App.css';
 import './interfaces.tsx';
 import ImportFile from './ImportFile.tsx';
 import { useState } from 'react';
 import { EstructuraDeMemoria, MemoriaFisica } from './interfaces.tsx';
 import MemoryForm from './MemoryForm.tsx';
+import LecturaDeDatos from './OrdenamientoDeMemoria.tsx';
 
 const convertirAPequenos = (valor: number) => {
   return valor * 1024;
@@ -45,14 +45,17 @@ function App() {
 
       <ImportFile onImport={set} />
 
-      
+      <LecturaDeDatos 
+      MemoriaVirtual={MemoriaFisicaLocal.memoryValues}
+      EstructuraDeMemoria={EstructuraDeMemoriaLocal}
+      />
 
-      <div>
+      <div className='container'>
         <h2>Memoria Estructura</h2>
-        <p><b>Tamano De Pagina: </b>{EstructuraDeMemoriaLocal.tamano_de_pagina} bytes</p>
-        <p><b>Tamano de Memoria Virtual: </b>{EstructuraDeMemoriaLocal.tamano_de_virtual} kb = {convertirAPequenos(EstructuraDeMemoriaLocal.tamano_de_virtual)} bytes = {Math.log2(convertirAPequenos(EstructuraDeMemoriaLocal.tamano_de_virtual))} bits</p>
-        <p><b>Tamano de Memoria Fisica: </b>{EstructuraDeMemoriaLocal.tamano_de_fisica} kb = {convertirAPequenos(EstructuraDeMemoriaLocal.tamano_de_fisica)} bytes = {Math.log2(convertirAPequenos(EstructuraDeMemoriaLocal.tamano_de_fisica))} bits</p>
-        <p><b>Numero de Paginas: </b>{EstructuraDeMemoriaLocal.numero_de_paginas}</p>
+        <p><b>Tamano De Pagina: </b><code>{EstructuraDeMemoriaLocal.tamano_de_pagina} bytes</code></p>
+        <p><b>Tamano de Memoria Virtual: </b><code>{EstructuraDeMemoriaLocal.tamano_de_virtual} kb = {convertirAPequenos(EstructuraDeMemoriaLocal.tamano_de_virtual)} bytes = {Math.log2(convertirAPequenos(EstructuraDeMemoriaLocal.tamano_de_virtual))} bits</code></p>
+        <p><b>Tamano de Memoria Fisica: </b><code>{EstructuraDeMemoriaLocal.tamano_de_fisica} kb = {convertirAPequenos(EstructuraDeMemoriaLocal.tamano_de_fisica)} bytes = {Math.log2(convertirAPequenos(EstructuraDeMemoriaLocal.tamano_de_fisica))} bits</code></p>
+        <p><b>Numero de Paginas: </b><code>{EstructuraDeMemoriaLocal.numero_de_paginas}</code></p>
         <h2>Memoria Fisica</h2>
         <table>
           <thead>
